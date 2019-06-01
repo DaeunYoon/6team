490bd81ed3dfcfacdc80e8b83059061580d044d7
0verc10ck <jph04135@naver.com>
2019-06-02 04:01:42 +0900

    문준범
    Fix DB Structure and Added SampleData by SQL

---
+++ Acoount_SampleData.sql
@@ -0,0 +1,5 @@
+SELECT * FROM roomshare.account;
+INSERT INTO `roomshare`.`account` (`ID`, `PW`, `Username`, `state`, `report`, `evaluation`, `PhoneNumber`) VALUES ('jph04135@naver.com', 'jhh10218!!', '0verc10ck', '0', '0', '0', '01035661704');
+INSERT INTO `roomshare`.`account` (`ID`, `PW`, `Username`, `state`, `report`, `evaluation`, `PhoneNumber`) VALUES ('abc@gmail.com', 'abcde', 'abc', '0', '0', '0', '01011111111');
+INSERT INTO `roomshare`.`account` (`ID`, `PW`, `Username`, `state`, `report`, `evaluation`, `PhoneNumber`) VALUES ('daeun@naver.com', 'yoon', 'daeun', '1', '0', '0', '01022222222');
+INSERT INTO `roomshare`.`account` (`ID`, `PW`, `Username`, `state`, `report`, `evaluation`, `PhoneNumber`) VALUES ('test@naver.com', 'test', 'tester', '1', '0', '0', '00103333333');
---
+++ Room_Address_SampleData.sql
@@ -0,0 +1,4 @@
+SELECT * FROM roomshare.room_address;
+INSERT INTO `roomshare`.`room_address` (`RoomID`, `add1`, `add2`, `add3`, `add4`) VALUES ('1', '대구광역시', '북구', '대현동', '대현로 19길 68-20');
+INSERT INTO `roomshare`.`room_address` (`RoomID`, `add1`, `add2`, `add3`, `add4`) VALUES ('2', '창원시', '진해구', '풍호동 ', '마린푸르지오');
+INSERT INTO `roomshare`.`room_address` (`RoomID`, `add1`, `add2`, `add3`, `add4`) VALUES ('3', '경상남도', '밀양시', '가곡동', '용두멘션');
